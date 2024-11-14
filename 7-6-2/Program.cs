cbfa009d054b437913a8afc952f9f71695bea30d
Giorf81 <rejroberts7@gmail.com>
2024-11-14 10:33:27 +0300

    Добавьте файлы проекта.

---
+++ 7-6-2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _7_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Car<T>
+    {
+        public T Engine;
+    }
+
+    class ElectricEngine
+    { }
+
+    class GasEngine
+    { }
+
+
+}
